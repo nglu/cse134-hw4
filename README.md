199b02cb1fe72c8e6e308311269edd94d8f154e3
Nguyen Luong <luongngocthaonguyen2309@gmail.com>
2022-07-27 22:27:31 -0700

    complete DELET

--- README.md
+++ README.md
@@ -8,6 +8,6 @@ You can access all parts of HW4 from any main pages (Home page, Projects, Contac
 
 - Part 1: completed
 - Part 2: completed
-- Part 3: partially completed
+- Part 3: partially completed (missing UPDATE)
 - Part 4: not really doing much, all the styling are based on stylesheets from last HW
 - Part 5: small attempt on DOMPurify
--- hw4/blog.js
+++ hw4/blog.js
@@ -2,7 +2,6 @@
  * JS file for CRUD
  */
 
-
 /**
  * @typedef {{
  *  title: string,
@@ -14,6 +13,9 @@
 /** LEARNING CORNER
  * 
  * Typing /** will give you this type of comments
+ * 
+ * var variable = variable  ||  {} ; <- curly braces mean empty object
+ * https://stackoverflow.com/questions/15844213/what-does-variable-variable-mean-in-javascript
  * 
  * The keyword "export" is kinda like public class, meaning you can access it
  * if you don't have it, you basically cannot link the script tag in html to this .js
@@ -30,17 +32,17 @@ export class PostSummary extends HTMLElement {}
  */
 export function defineBlogElements() {
     console.info("Registering custom elements... ");
-
     customElements.define("post-article", PostArticle, {extends: 'div'});
-
     customElements.define("post-title", PostTitle, {extends: 'div'});
-
     customElements.define("post-summary", PostSummary, {extends: 'div'});
-};
+}; /* defineBlogElements */
 
+/**
+ * Create a random ID
+ */
 export function generateId() {
     return crypto.randomUUID();
-};
+}; /* generateId */
 
 export const examplePosts = [
     {
@@ -66,6 +68,14 @@ export const examplePost = {
     "summary": "When we are building APIs, we want our models to provide four basic types of functionality. The model must be able to Create, Read, Update, and Delete resources. Computer scientists often refer to these functions by the acronym CRUD. A model should have the ability to perform at most these four functions in order to be complete. If an action cannot be described by one of these four operations, then it should potentially be a model of its own. - CodeAcademy"
 };
 
+/**
+ * Returns the number of posts in the DB
+ */
+export function countBlogPosts() {
+    let blogPosts = loadBlogPosts();
+    return Object.keys(blogPosts).length;
+} /* countBlogPosts */
+
 
 /**
  * @param {string} [blogPostId]
@@ -77,29 +87,36 @@ export const examplePost = {
  * or post-article with [[Prototype]]: HTMLElement
  * Weird...
  * 
- * The return type is not really the problem, it might be about some event 
+ * The return type is not really the problem, it might be about some event timing issues
  */
 export function renderBlogPost(blogPostId, blogPost) {
-    /* Get the template */
+    // Get the template
     const template = document.getElementById('blog-template');
-    /* Create a copy of the template */
+    // Create a copy of the template
     const postElement = template.content.cloneNode(true);
-    /* Set a data- attribute with the value of ID */
+    // Set a data- attribute with the value of ID
     postElement.children[0].dataset.blogPostId = blogPostId;
-
+    // Get & set the blogpost.elements
     const titleH2 = postElement.querySelector('post-title > h2');
     titleH2.textContent = blogPost.title;
-
     const postDate = postElement.querySelector('post-title > p');
     postDate.textContent = blogPost.date;
-
     const postSummary = postElement.querySelector('post-summary > p');
     postSummary.textContent = blogPost.summary;
 
-    console.log(postElement.children[0]);
+    //console.log(postElement.children[0]);
+    
+    const deleteButton = postElement.querySelector('post-article > button');
+    deleteButton.addEventListener ('click', (ev) => {  // you can check out ev.path
+        deletePost(blogPostId);
+        let selectedPost = ev.path[1];
+        selectedPost.remove();
+        //window.location.reload();   // this might be weird in the future when working with DB API but it works now
+    });
 
+    
     return postElement;
-}
+} /* renderBlogPost */
 
 /**
  * @param {string} [blogPostId]
@@ -113,6 +130,11 @@ export function displayBlogPost(blogPostId, blogPost, container) {
     // unchanged compared to the one we'd be replacing it with to
     // avoid unnecessary changes to the DOM.
 
+    const existingPost = container.querySelector(`[data-blog-post-id="${blogPostId}"]`);
+    if (existingPost) {
+        existingPost.remove();
+    }
+
     /**
      * Check if the user input anything
      * Should enhance more, this will accept an input with only 
@@ -122,15 +144,26 @@ export function displayBlogPost(blogPostId, blogPost, container) {
     if (blogPost) {
         container.appendChild(postElement);
     }
-}
-
-/* Storage Layer
- * ============= */
+} /* displayBlogPost */
 
 /**
- * @returns the JSON map of posts from local storage.
+ * Displaying all blog posts
  */
- function loadBlogPosts() {
+export function displayAllBlogPosts (container) {
+    const blogPosts = selectAllPosts();
+
+    for (const [id, post] of Object.entries(blogPosts)) {
+        displayBlogPost(id, post,container);
+    }
+} /* displayAllBlogPosts */
+
+/* Storage Layer
+ * ============================================================== */
+
+/**
+ * return the JSON map of posts from local storage.
+ */
+function loadBlogPosts() {
     return JSON.parse(localStorage.getItem('blogPosts')) || {};
 } /* loadPosts */
 
@@ -142,20 +175,62 @@ function storeBlogPosts(blogPosts) {
     localStorage.setItem('blogPosts', JSON.stringify(blogPosts));
 } /* storePosts */
 
+
 /* Crud Operations Layer
- * ===================== */
+ * ============================================================== */
 
 /**
  * (CREATE)
- * @param {BlogPost} [blogPost] the book to be added to the DB.
- * @returns {String} the generated UUID for this book in the DB.
+ * @param {BlogPost} [blogPost] the post to be added to the DB.
+ * @returns {String} the generated UUID for this post in the DB.
  */
 export function createPost(blogPost) {
     const blogPosts = loadBlogPosts();
     const postId = generateId();
+
+    console.log(`postId: ${postId}`);
 
     blogPosts[postId] = blogPost;
     storeBlogPosts(blogPosts);
     
     return postId;
 } /* createPost */
+
+/**
+ * (READ) Read a specific post from DB.
+ * @param {string} [blogPostId] the id of the book to look up. 
+ * @returns {BlogPost | undefined} the post found, or undefined if not found.
+ */
+export function selectOnePost(blogPostId) {
+    const blogPosts = loadBlogPosts();
+    return blogPosts[blogPostId];
+} /* selectOnePost */
+
+/**
+ * (READ) Return _all_ posts from the DB.
+ * @returns {{str: BlogPost}}
+ */
+ export function selectAllPosts() {
+    const blogPosts = loadBlogPosts();
+    return blogPosts;
+} /* selectAllPosts */
+
+/**
+ * (UPDATE)
+ */
+
+/**
+ * (DELETE)
+ * @param {string} [postID] ID of the post selected to be delete
+ * @returns {bool} true if we deleted an element, false if not.
+ */
+ export function deletePost(postID) {
+    const blogPosts = loadBlogPosts();
+
+    // If it's not there, just return false. If it is there, delete it.
+    if (!(postID in blogPosts)) return false;
+    delete blogPosts[postID];
+
+    storeBlogPosts(blogPosts);
+    return true;
+}
